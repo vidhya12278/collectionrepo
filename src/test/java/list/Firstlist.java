83a0bc22d35d15646c06e147742a3ee1032f1b64
vidhya12278 <evidhya60@gmail.com>
2020-05-28 07:10:19 +0530

    collection list

---
+++ src/test/java/list/Firstlist.java
@@ -0,0 +1,24 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Firstlist {
+public static void main(String[] args) {
+	List<Integer> l=new ArrayList<Integer>();
+	
+	l.add(10);
+	l.add(30);
+	l.add(60);
+	
+	System.out.println("all list values : " + l);
+	
+	int s = l.size();
+	System.out.println("list size: " + s);
+	
+	
+
+
+
+}
+}
